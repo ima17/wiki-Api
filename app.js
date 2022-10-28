7b23dc93920486550be85afad008c910f27d19e6
ima17 <ima.sudaraka@outlook.com>
2022-10-28 07:53:16 +0530

    Create delete post route

--- app.js
+++ app.js
@@ -44,6 +44,16 @@ app.post("/articles", function (req, res) {
   });
 });
 
+app.delete("/articles", function (req, res) {
+  Article.deleteMany(function (err) {
+    if (!err) {
+      res.send("Successfully deleted the articles");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server Started successfully");
 });
